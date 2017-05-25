133e08a60f01daa4f5d3a16f9f480054821211eb
Sharan Khan <sharan.gohar@gmail.com>
2017-05-25 23:59:12 +0500

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
-  styles: ['h1 { color: blue; }']
+  styles: ['.logsClass { color: white;background:blue }']
 })
 export class AppComponent {
-   username:string='';
-   
+   username:string = '';
+   showDetails:boolean = false;
+   log=[]
    constructor()
     {
         console.log("App Component called");
+    }
+    updateDetails()
+    {
+      this.showDetails = this.showDetails==false ? true : false;
+      this.log.push(this.log.length+1);
     }
 }
