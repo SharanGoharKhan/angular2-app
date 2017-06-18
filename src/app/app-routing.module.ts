1d93b2882679c8c355d45a46cf66c08e2dacd3fd
Sharan Khan <sharan.gohar@gmail.com>
2017-06-18 19:07:02 +0500

    working on authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,15 +11,17 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { SignupComponent } from './auth/signup/signup.component';
 const appRoutes: Routes = [
     {path:'',redirectTo:'/recipes', pathMatch:'full'},
     {path:'recipes',component:RecipesComponent, children: [
         {path: '',component: RecipeStartComponent},
         {path: 'new', component:RecipeEditComponent},
         {path:':id',component:RecipeDetailComponent},
-        {path: ':id/edit', component:RecipeEditComponent}
+        {path: ':id/edit', component:RecipeEditComponent},
     ]},
-    {path:'shopping-list',component:ShoppingListComponent}
+    {path:'shopping-list',component:ShoppingListComponent},
+    {path: 'signup', component:SignupComponent}
 ];
 @NgModule(
     {
